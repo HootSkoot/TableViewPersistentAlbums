47e573a9a947706e853e3c1c27d3971a6ae9d77c
user156499 <user156499@74-80-245-197.ic2net.net>
2020-06-01 21:26:03 -0700

    Initial Commit

---
+++ TableViewAppM1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewAppM1
+//
+//  Created by user156499 on 6/1/20.
+//  Copyright © 2020 MZN0047. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
